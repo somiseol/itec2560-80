ac46ae0c26d2053fb11cc0593adc802f4242fefe
somiseol <somi.seol@proton.me>
2025-10-20 13:07:05 -0500

    project 1

--- lab8-webapp/routes/index.js
+++ lab8-webapp/routes/index.js
@@ -5,4 +5,19 @@ router.get('/', function(req, res, next) {
     res.render('index', {title: 'Feedback app', author: 'somi'})
 })
 
+router.get('/feedback-form', function(req, res, next){
+    res.render('student_feedback_form')
+})
+
+router.post('/submit-feedback', function(req, res, next){
+    const formData = req.body
+
+    res.render('thank you', {
+        name: formData.name,
+        email: formData.email,
+        message: formData.comments,
+        currentStudent: formData['current-student']
+    })
+})
+
 module.exports = router
--- lab8-webapp/server.js
+++ lab8-webapp/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const staticFileLocation = path.join(__dirname, 'public')
+app.use(express.static(staticFileLocation))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
