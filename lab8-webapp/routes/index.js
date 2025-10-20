77f602513cef6d090806011717b1f8b523b9a4dd
somiseol <somi.seol@proton.me>
2025-10-20 14:36:41 -0500

    lab8 part1 done

--- lab8-webapp/routes/index.js
+++ lab8-webapp/routes/index.js
@@ -9,15 +9,21 @@ router.get('/feedback-form', function(req, res, next){
     res.render('student_feedback_form')
 })
 
+
+
 router.post('/submit-feedback', function(req, res, next){
+    // get form data
     const formData = req.body
 
-    res.render('thank you', {
+    //TODO: save data to database
+
+    res.render('thank_you', {
         name: formData.name,
         email: formData.email,
         message: formData.comments,
         currentStudent: formData['current-student']
     })
 })
+
 
 module.exports = router
--- lab8-webapp/server.js
+++ lab8-webapp/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+// import from index.js
 const indexRouter = require('./routes/index')
 
-
+// create web app
 const app = express()
 
+// parse req form body
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const staticFileLocation = path.join(__dirname, 'public')
@@ -17,6 +19,7 @@ app.set('view engine', 'hbs')
 
 app.use('/', indexRouter)
 
-let server = app.listen(process.env.PORT || 3000, function (){
+// start server
+let server = app.listen(process.env.PORT || 3000, function () {
     console.log('Express server running on port', server.address().port)
 })
