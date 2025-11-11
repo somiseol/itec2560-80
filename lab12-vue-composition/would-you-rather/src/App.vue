7844354c888f0b552ef7e63a3761f30f1bb6172f
somiseol <somi.seol@proton.me>
2025-11-11 12:19:24 -0600

    lab12

--- lab12-vue-composition/would-you-rather/src/App.vue
+++ lab12-vue-composition/would-you-rather/src/App.vue
@@ -1,30 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { ref } from 'vue'
+
+import WouldYouRather from './components/WouldYouRather.vue';
+
+// reactive state (internally belongs to component, scoped w/in itself)
+const wyrQuestion = ref('eat an apple or eat a worm')
+const wyrAnswer1 = ref('apple')
+const wyrAnswer2 = ref('worm')
+const userSelection = ref('')
+
+function updateUserSelection(userChoice) {
+  userSelection.value = `you chose: ${userChoice}`
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>would you rather:</h1>
+
+  <WouldYouRather
+    v-bind:question="wyrQuestion" 
+    v-bind:answer1="wyrAnswer1" 
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-selected="updateUserSelection">
+  </WouldYouRather>
+
+  <h2>{{ userSelection}}</h2>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ lab12-vue-composition/would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,44 @@
+<script setup>
+  import { ref, watch } from 'vue'
+
+  // props: values from parent to child
+  defineProps({
+    question: String,
+    answer1: String,
+    answer2: String
+  })
+
+  const choice = ref('')
+
+  // emits: values from child to parent
+  const emit = defineEmits( [
+    'answer-selected' // name of event
+  ])
+
+  // "watch" instead of "v-on:change"
+  watch(choice, () => {
+    emit('answer-selected', choice.value)
+  })
+
+</script>
+
+<template>
+
+  <h2>make a choice</h2>
+
+  {{ question }}
+
+  <div>
+    <!-- radio to one var -->
+    <input type="radio" id="answer-1" v-model="choice" v-bind:value="answer1">
+    <label for="answer-1">{{ answer1 }}</label>
+
+    <input type="radio" id="answer-2" v-model="choice" v-bind:value="answer2">
+    <label for="answer-2">{{ answer2 }}</label>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
