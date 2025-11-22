5a3b9b19eec2521f388703eb85bd7efc0c67d753
somiseol <somi.seol@proton.me>
2025-11-21 23:45:01 -0600

    finished bmi calculator

---
+++ lab12-vue-composition/bmicalc/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import BmiCalc from './components/bmiCalc.vue'
+
+const bmiAnswer = ref('')
+
+const heightQuestionMetric = ref('height in m ')
+const weightQuestionMetric = ref('weight in kg ')
+
+const heightQuestionImperial = ref('height in in ')
+const weightQuestionImperial = ref('weight in lb ')
+
+const isImperial = ref(false)
+
+// ???
+function updateBmi(bmi) {
+    bmiAnswer.value = bmi
+}
+</script>
+
+<template>
+    <h1>BMI CALCULATOR</h1>
+
+    <label for="imperialCheck">imperial</label>
+    <input id="imperialCheck" type="checkbox" v-model="isImperial">
+
+    <BmiCalc
+        v-bind:heightQ="isImperial ? heightQuestionImperial : heightQuestionMetric"
+        v-bind:weightQ="isImperial ? weightQuestionImperial : weightQuestionMetric"
+
+        v-bind:isImperial="isImperial"
+
+        v-on:bmiAnswer="updateBmi"
+    /> 
+
+    <div v-if="bmiAnswer">
+        <p>BMI: {{ bmiAnswer }}</p>
+    </div>
+
+</template>
---
+++ lab12-vue-composition/bmicalc/src/components/bmiCalc.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+
+const inHeight = ref(0)
+const inWeight = ref(0)
+
+// from parent to child
+defineProps({
+    heightQ: String,
+    weightQ: String,
+
+    isImperial:Boolean
+})
+
+const emit = defineEmits ( [
+    'bmiAnswer'
+    ] )
+
+function calculateAndEmit(isImperial) {
+    let bmi = 0 // def value
+    
+    if (inHeight.value > 0 && inWeight.value > 0 && isImperial) { // bmi formula
+        bmi = inWeight.value / (inHeight.value * inHeight.value) * 730 // asign val
+    }
+    else if (inHeight.value > 0 && inWeight.value > 0 && !isImperial)
+    {
+        bmi = inWeight.value / (inHeight.value * inHeight.value)
+    }
+
+    emit('bmiAnswer', bmi.toFixed(2)) // emit(var, val)
+}
+
+</script>
+
+
+<template>
+    <div>
+        <h2>Enter height and weight:</h2>
+     </div>
+
+    <div>
+        <label for="inHeight">{{ heightQ }} </label>
+        <input id="inHeight" type="number" v-model="inHeight"> <!-- 2-way -->
+        <br>
+        <label for="inWeight">{{ weightQ }} </label>
+        <input id="inWeight" type="number" v-model="inWeight">
+    </div>
+
+    <br>
+    <button v-on:click="calculateAndEmit(isImperial)">Calculate</button>
+</template>
